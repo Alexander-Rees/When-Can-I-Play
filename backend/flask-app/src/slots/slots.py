7393985cbd74bb48ad01ae470720cdf770eb8b7c
garrettladley <ladley.g@northeastern.edu>
2023-10-22 12:33:27 -0400

    db cursor example

--- backend/flask-app/src/slots/slots.py
+++ backend/flask-app/src/slots/slots.py
@@ -1,8 +1,14 @@
-from flask import Blueprint
+from flask import Blueprint, make_response, jsonify, request
+from src import db
 
 slots = Blueprint('slots', __name__)
 
 
 @slots.route('/all', methods=['GET'])
 def all_slots() -> list:
-    return []
+    cursor = db.get_db().cursor()
+    cursor.execute('SELECT * FROM slots')
+    data = cursor.fetchall()
+    response = make_response(jsonify(data))
+    response.status_code = 200
+    return response
