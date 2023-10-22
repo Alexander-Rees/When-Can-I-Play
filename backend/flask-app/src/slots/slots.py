337b22ed1b1811ab4a93dfc6434c902ca63f3cd4
codyho22 <codyho168@gmail.com>
2023-10-22 13:48:19 -0400

    initial

--- backend/flask-app/src/slots/slots.py
+++ backend/flask-app/src/slots/slots.py
@@ -18,5 +18,17 @@ def all_slots() -> list:
 # read route
 
 # update route
+@slots.route('/update/<int:slotID>', methods=['PUT']) #might need to be slotID without _
+def update_slot(slotID):
+    data = request.get_json()
+
+    # Update the slot in the database
+    cursor = db.get_db().cursor()
+    cursor.execute('UPDATE slots SET column1 = ?, column2 = ? WHERE id = ?', (data['value1'], data['value2'], slotID))
+    db.get_db().commit()
+
+    response = make_response(jsonify({'message': 'Slot updated successfully'}))
+    response.status_code = 200
+    return response
 
 # delete route
