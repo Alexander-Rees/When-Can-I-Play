9f85f3c9bbafacb20d02ae49810d2c5d8d9d39fd
AlexRees23 <alexander.rees@gmail.com>
2023-11-12 14:00:30 -0500

    Added to Home page

--- frontend/src/app/page.tsx
+++ frontend/src/app/page.tsx
@@ -6,40 +6,44 @@ import Link from 'next/link'
 import { useEffect, useState } from 'react';
 
 export default function Home() {
-  const [data, setData] = useState("");
-  const [count, setCount] = useState(0)
+  // Mock data
+  const mockData = {
+    "use client": [
+      { name: 'Carter 2B', timeTill: '4:30pm', distance: '0.7 mi' },
+      { name: 'Roxbury 2S', timeTill: '3:30pm', distance: '0.9 mi' },
+      // ... other mock location data
+    ]
+  };
 
-  const getData = async () => {
-    const resp = await axios.get('https://api.sampleapis.com/coffee/hot');
-    const json = await resp.data;
-    setData(json);
-    setCount(count + 1)
-    console.log(json)
-  }
+  const [data, setData] = useState(mockData); // Set default state to mock data
+  const [count, setCount] = useState(0);
 
   useEffect(() => {
-    getData();
+    // Now we just set the data without fetching from an API
+    setData(mockData);
+    setCount(count + 1);
+    console.log(mockData);
   }, []);
 
   return (
-    <div className="flex">
+    <div className="flex h-screen bg-white"> {/* Make the entire background white and extend the full height of the screen */}
       {/* Sidebar */}
-      <div className="w-64 bg-red-500 text-white p-4"> {/* Adjust width and padding as needed */}
+      <div className="w-64 bg-red-500 text-white p-4 fixed h-full"> {/* Make sidebar fixed and full height */}
         {/* Sidebar content */}
       </div>
 
       {/* Main content */}
-      <div className="flex-1 p-4">
+      <div className="flex-1 p-4 ml-64"> {/* Add left margin to make room for the sidebar */}
         {/* Header */}
         <div className="flex justify-between items-center mb-4">
           <h1 className="text-xl font-bold">Home</h1>
           <div>
             {/* Header buttons */}
-            <Link href="/when-can-I" className="bg-black text-white rounded-full px-4 py-2 mr-2">
+            <Link href="/report" className="bg-black text-white rounded-full px-4 py-2 mr-2">
               Report Field Activity
             </Link>
 
-            <Link href="/report" className="bg-black text-white rounded-full px-4 py-2 inline-block">
+            <Link href="/when-can-I" className="bg-black text-white rounded-full px-4 py-2 inline-block">
               When Can I Play?
             </Link>
           </div>
@@ -47,16 +51,20 @@ export default function Home() {
 
         {/* Cards */}
         <div className="grid grid-cols-3 gap-4">
-          {/* Each card */}
-          <div className="bg-gray-100 p-4 rounded-lg">
-            <h2 className="text-lg font-semibold">Closest Open Locations:</h2>
-            {/* Locations list */}
-          </div>
-          {/* Repeat for other cards */}
+          {/* Each card for locations */}
+          {data['use client'].map((location, index) => (
+            <div key={index} className="bg-gray-100 p-4 rounded-lg">
+              <h2 className="text-lg font-semibold">{location.name}</h2>
+              <p>Time Till: {location.timeTill}</p>
+              <p>Distance: {location.distance}</p>
+            </div>
+          ))}
         </div>
 
         {/* Footer or other content */}
       </div>
     </div>
   );
+
 }
+
