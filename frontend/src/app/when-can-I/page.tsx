77e049434816b623fb4d649f2f241c601d1abfed
codyho22 <codyho168@gmail.com>
2023-11-12 14:13:16 -0500

    added code for my calendar

--- frontend/src/app/when-can-I/page.tsx
+++ frontend/src/app/when-can-I/page.tsx
@@ -1,10 +1,16 @@
-export default function when() {
+// Calendar.js
+import React from 'react';
+import Calendar from 'react-calendar';
+import 'react-calendar/dist/Calendar.css';
+import './Calendar.css'; // Import your custom CSS file
 
-    return (
-        <div>
-            <h1>When Can I Play</h1>
-        </div>
+const EmptyCalendar = () => {
+  return (
+    <div>
+      <h1>When Can I Play</h1>
+      <Calendar />
+    </div>
+  );
+};
 
-    )
-
-}
+export default EmptyCalendar;
