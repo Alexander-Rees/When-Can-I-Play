9ac53333f04f7602f171c0bb87052ddaf224e928
Alexander <70789477+AlexSchinkmannB@users.noreply.github.com>
2023-11-12 14:36:08 -0500

    Functionality to report page

--- frontend/src/app/report/page.tsx
+++ frontend/src/app/report/page.tsx
@@ -1,10 +1,46 @@
+"use client"
+
+import { useState } from "react";
+
 export default function Report() {
+    const [report, setReport] = useState("");
+    const [reportList, setReportList] = useState<Array<string>>([])
+
+
+    // const reportButton = document.getElementById("reportBtn") as HTMLInputElement
+    // const printedReport = document.getElementById("reportedOutput") as HTMLInputElement
+
+
+    // function displayReport(): void {
+    //     const initInput = input.toString(); 
+    //     printedReport.textContent = initInput; 
+    // }
+
+    // reportButton.addEventListener("click", displayReport); 
+    console.log(reportList)
+
+    const handleUpdateList = () => {
+        setReportList(arr => [...arr, report]);
+        setReport("")
+    }
 
     return (
         <div>
-            <h1>Report</h1>
+        <h1>User Reports Page</h1>
+        <p>Enter Your Own Report:</p>
+        <form onSubmit={(e) => {
+            e.preventDefault()
+            handleUpdateList}
+        }>
+        <input type="string" id="userInput" placeholder="report..." value={report} onChange={(e) => {
+            setReport(e.target.value)
+        }}/>
+        <button id="reportBtn" onClick={handleUpdateList}>Enter</button>
+        </form>
+        <h3>You entered: <output id="reportedOutut">{reportList.map((reportItem) => (
+            <p>{reportItem}</p>
+        ))}</output></h3>
         </div>
 
-    )
-
+    ); 
 }
